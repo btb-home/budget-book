09921408c6edddccd7864ed0c9a06ba21ec7482b
handal95 <handal0131@gmail.com>
2025-02-02 22:59:30 +0900

    feat: launcher

--- launcher.bat
+++ launcher.bat
@@ -1,12 +1,4 @@
-# budget-book
-BTB budget book project
-
-# Docker 
 docker build -t btbdocker/budget-book:0.0.1 -f docker/Dockerfile .
 docker stop poc
 docker rm poc
 docker run -d -p 80:8030 --name poc btbdocker/budget-book:0.0.1
-
-docker push btbdocker/budget-book:0.0.1
-
-docker pull btbdocker/budget-book:0.0.1
